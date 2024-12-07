f20a3411d4c6243233bf47081b3b0cdf4319aa64
Raul Bermudez <raulbego16@gmail.com>
2024-12-07 15:58:37 +0000

    Añado cambios en mascotas de clases

---
+++ EjerciciosPHP/Ejemplos/ud5/clases/mascotas/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2c253bb393df64a556b47b14c8165d24051cf7df',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2c253bb393df64a556b47b14c8165d24051cf7df',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
